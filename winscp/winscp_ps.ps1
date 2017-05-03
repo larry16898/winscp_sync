73371d9b3f4428e9e58a9b7d1eae75310124bacd
larry <larry@microsoft.com>
2017-05-03 17:17:27 +0800

    add create shortcut

--- winscp/winscp_ps.ps1
+++ winscp/winscp_ps.ps1
@@ -1,5 +1,5 @@
 # Load WinSCP .NET assembly
-Add-Type -Path "WinSCPnet.dll"
+#Add-Type -Path "WinSCPnet.dll"
  
 # Session.FileTransferred event handler
  
@@ -52,47 +52,101 @@ function FileTransferred
     }
 }
 
-try
+function SyncStuff
 {
-    # Load WinSCP .NET assembly
-    Add-Type -Path "WinSCPnet.dll"
- 
-    # Setup session options
-    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
-        Protocol = [WinSCP.Protocol]::Sftp
-        HostName = "192.168.56.102"
-        UserName = "root"
-        Password = "abc@123"
-        SshHostKeyFingerprint = "ssh-rsa 2048 d9:a9:62:39:33:41:30:06:13:cb:3b:86:e8:e3:da:33"
-    }
- 
-    $session = New-Object WinSCP.Session
-
+param(
+    [string]$HostName='192.168.56.102',
+    [string]$UserName='updateuser',
+    [string]$Password='abc@123',
+    [string]$SshHostKeyFingerprint='ssh-rsa 2048 d9:a9:62:39:33:41:30:06:13:cb:3b:86:e8:e3:da:33',
+    [string]$local_path='C:\Program Files\tbtools',
+    [string]$remote_path='/updatesource/cs_office'
+)
     try
     {
-        # Will continuously report progress of synchronization
-        $session.add_FileTransferred( { FileTransferred($_) } )
+        # Load WinSCP .NET assembly
+        Add-Type -Path "WinSCPnet.dll"
+
+        # determine if the local destination folder is exists.
+        $local_path = 'C:\Program Files\tbtools'
+        if(!(Test-Path $local_path))
+        {
+            New-Item -Path $local_path -ItemType Directory -ErrorAction Stop >$null
+        }
  
-        # Connect
-        $session.Open($sessionOptions)
+        # Setup session options
+        $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+            Protocol = [WinSCP.Protocol]::Sftp
+            HostName = $HostName
+            UserName = $UserName
+            Password = $Password
+            SshHostKeyFingerprint = $SshHostKeyFingerprint
+        }
  
-        # Synchronize files
-        $synchronizationResult = $session.SynchronizeDirectories(
-            [WinSCP.SynchronizationMode]::Local, "c:\temp", "/root/Downloads", $True)
+        $session = New-Object WinSCP.Session
+
+        try
+        {
+            # Will continuously report progress of synchronization
+            $session.add_FileTransferred( { FileTransferred($_) } )
  
-        # Throw on any error
-        $synchronizationResult.Check()
+            # Connect
+            $session.Open($sessionOptions)
+ 
+            # Synchronize files
+            $synchronizationResult = $session.SynchronizeDirectories(
+                [WinSCP.SynchronizationMode]::Local, $local_path, $remote_path, $True)
+ 
+            # Throw on any error
+            $synchronizationResult.Check()
+
+            Write-Host -ForegroundColor Green -BackgroundColor Black "同步成功完成！"
+        }
+        finally
+        {
+            # Disconnect, clean up
+            $session.Dispose()
+        }
+
+ #       exit 0
     }
-    finally
+    catch [Exception]
     {
-        # Disconnect, clean up
-        $session.Dispose()
+        Write-Host ("Error: {0}" -f $_.Exception.Message)
+        exit 1
     }
- 
-    exit 0
+
 }
-catch [Exception]
+
+function CreateShortCut
 {
-    Write-Host ("Error: {0}" -f $_.Exception.Message)
-    exit 1
+    param(
+        [string]$shortcutName,
+        [string]$programName,
+        [string]$installPath
+    )
+    try
+    {
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$home\desktop\$shortcutName")
+    $Shortcut.TargetPath = Join-Path $installPath $programName
+    $Shortcut.WorkingDirectory = $installPath
+    $Shortcut.Save()
+    }
+    catch [Exception]
+    {
+        Write-Host ("Error: {0}" -f $_.Exception.Message)
+        exit 1
+    }
+}
+
+# write my own code here
+$tb_tools_list = @(@{programName='skype_helper.exe';installPath='C:\Program Files\tbtools\skype_helper';shortcutName='客服Skype登录助手.lnk'})
+
+SyncStuff -HostName '192.168.56.102' -UserName 'updateuser' -Password 'abc@123' -SshHostKeyFingerprint 'ssh-rsa 2048 d9:a9:62:39:33:41:30:06:13:cb:3b:86:e8:e3:da:33' `
+                -local_path 'C:\Program Files\tbtools' -remote_path '/updatesource/cs_office'
+
+foreach ($tool in $tb_tools_list)
+{
+    CreateShortCut -shortcutName $tool.shortcutName -programName $tool.programName -installPath $tool.installPath
 }
